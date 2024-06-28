d469f0d2cff67eacd89e250185b27d41751d180f
Archisman <archismanmukherjee500@gmail.com>
2024-06-28 11:15:42 +0530

    New Project

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use('/students', studentRoute)
 
 // PORT
 const port = process.env.PORT || 4000;
-const server = app.listen(port,
+app.listen(port,
 	() => {
 		console.log('Connected to port ' + port)
 	})
